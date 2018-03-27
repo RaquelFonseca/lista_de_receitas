833ffb70982665cc5116627f9901c2e10c57abe0
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2018-03-27 09:57:49 -0300

    :)

--- veganizze/config/routes.rb
+++ veganizze/config/routes.rb
@@ -3,3 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
+Rails.application.routes.draw do
+	resources :recipes
+	root 'recipes#index'
+
+end
+
